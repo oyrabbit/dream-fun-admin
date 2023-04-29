da5a27f9ae15c45b803b2906d8b4c6ff858932ca
oyrabbit <486163408@qq.com>
2023-04-30 03:46:52 +0800

    feat: 后端修改

--- api/v1/web.go
+++ api/v1/web.go
@@ -1,13 +1,6 @@
 package v1
 
 import "C"
-import (
-	"github.com/gin-gonic/gin"
-	"github.com/oyrabbit/dream-fun-admin/model"
-	"github.com/oyrabbit/dream-fun-admin/utils/errmsg"
-	"net/http"
-	"strconv"
-)
 
 type CustomWebsiteList struct {
 	UserCategoryID   uint   `gorm:"primary_key;auto_increment" json:"user_category_id"`
@@ -100,30 +93,30 @@ type CustomWebsiteList struct {
 //}
 
 // EditCate 编辑分类名
-func EditUserWebsite(c *gin.Context) {
-	var data model.Website
-	id, _ := strconv.Atoi(c.Param("id"))
-	_ = c.ShouldBindJSON(&data)
-	code := model.EditWebsite(id, &data)
-
-	c.JSON(
-		http.StatusOK, gin.H{
-			"status":  code,
-			"message": errmsg.GetErrMsg(code),
-		},
-	)
-}
-
-// DeleteCate 删除用户
-func DeleteUserWebsite(c *gin.Context) {
-	id, _ := strconv.Atoi(c.Param("id"))
-
-	code := model.DeleteWebsite(id)
-
-	c.JSON(
-		http.StatusOK, gin.H{
-			"status":  code,
-			"message": errmsg.GetErrMsg(code),
-		},
-	)
-}
+//func EditUserWebsite(c *gin.Context) {
+//	var data model.Website
+//	id, _ := strconv.Atoi(c.Param("id"))
+//	_ = c.ShouldBindJSON(&data)
+//	code := model.EditWebsite(id, &data)
+//
+//	c.JSON(
+//		http.StatusOK, gin.H{
+//			"status":  code,
+//			"message": errmsg.GetErrMsg(code),
+//		},
+//	)
+//}
+//
+//// DeleteCate 删除用户
+//func DeleteUserWebsite(c *gin.Context) {
+//	id, _ := strconv.Atoi(c.Param("id"))
+//
+//	code := model.DeleteWebsite(id)
+//
+//	c.JSON(
+//		http.StatusOK, gin.H{
+//			"status":  code,
+//			"message": errmsg.GetErrMsg(code),
+//		},
+//	)
+//}
--- api/v1/weisite.go
+++ api/v1/weisite.go
@@ -1,9 +1,11 @@
 package v1
 
+import "C"
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/oyrabbit/dream-fun-admin/model"
 	"github.com/oyrabbit/dream-fun-admin/utils/errmsg"
+	"gorm.io/gorm"
 	"net/http"
 	"strconv"
 )
@@ -123,6 +125,53 @@ func DeleteWebsite(c *gin.Context) {
 	c.JSON(
 		http.StatusOK, gin.H{
 			"status":  code,
+			"message": errmsg.GetErrMsg(code),
+		},
+	)
+}
+
+func EditUserWebsite(c *gin.Context) {
+	var data model.Website
+	id, _ := strconv.Atoi(c.Param("id"))
+	_ = c.ShouldBindJSON(&data)
+	code := model.EditWebsite(id, &data)
+
+	c.JSON(
+		http.StatusOK, gin.H{
+			"status":  code,
+			"message": errmsg.GetErrMsg(code),
+		},
+	)
+}
+
+// DeleteCate 删除用户
+func DeleteUserWebsite(c *gin.Context) {
+	id, _ := strconv.Atoi(c.Param("id"))
+
+	code := model.DeleteWebsite(id)
+
+	c.JSON(
+		http.StatusOK, gin.H{
+			"status":  code,
+			"message": errmsg.GetErrMsg(code),
+		},
+	)
+}
+
+func GetWebsites(c *gin.Context) {
+	var db *gorm.DB
+	//id, _ := strconv.Atoi(c.Param("id"))
+	var websiteList []CustomWebsiteList
+	//fmt.Println(id)
+	username := C.get("username")
+	db.Raw("SELECT t1.id as user_category_id,t1.`name` as user_category_name,t1.user_id,t2.id as user_website_id,t2.`name` as user_website_name, t2.url as user_website_url FROM `cate` as t1,`web` as t2,`user` as t3 WHERE t1.user_id=t3.id AND t1.id=t2.category_id and t3.username=?", username).Scan(&websiteList)
+	//data := model.GetCustomWebsite(id)
+	code := errmsg.SUCCSE
+	c.JSON(
+		http.StatusOK, gin.H{
+			"status": code,
+			"data":   websiteList,
+			//"total":   total,
 			"message": errmsg.GetErrMsg(code),
 		},
 	)
