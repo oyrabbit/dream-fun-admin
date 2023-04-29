3b3aeae9d542ad27819d3b424abdd4045432d81f
oyrabbit <486163408@qq.com>
2023-04-29 23:06:32 +0800

    feat: 后端修改

--- routes/router.go
+++ routes/router.go
@@ -80,22 +80,12 @@ func InitRouter() {
 		auth.POST("f_category/add", v1.AddFCategory)
 		auth.PUT("f_category/:id", v1.EditFCate)
 		auth.DELETE("f_category/:id", v1.DeleteFCate)
-		// 文章模块的路由接口
-		auth.GET("admin/article/info/:id", v1.GetArtInfo)
-		auth.GET("admin/article", v1.GetArt)
-		auth.POST("article/add", v1.AddArticle)
-		auth.PUT("article/:id", v1.EditArt)
-		auth.DELETE("article/:id", v1.DeleteArt)
+
 		// 上传文件
 		auth.POST("upload", v1.UpLoad)
 		// 更新个人设置
 		auth.GET("admin/profile/:id", v1.GetProfile)
 		auth.PUT("profile/:id", v1.UpdateProfile)
-		// 评论模块
-		auth.GET("comment/list", v1.GetCommentList)
-		auth.DELETE("delcomment/:id", v1.DeleteComment)
-		auth.PUT("checkcomment/:id", v1.CheckComment)
-		auth.PUT("uncheckcomment/:id", v1.UncheckComment)
 	}
 
 	/*
@@ -112,23 +102,12 @@ func InitRouter() {
 		router.GET("category", v1.GetCate)
 		router.GET("category/:id", v1.GetCateInfo)
 
-		// 文章模块
-		router.GET("article", v1.GetArt)
-		router.GET("article/list/:id", v1.GetCateArt)
-		router.GET("article/info/:id", v1.GetArtInfo)
-
 		// 登录控制模块
 		router.POST("login", v1.Login)
 		router.POST("loginfront", v1.LoginFront)
 
 		// 获取个人设置信息
 		router.GET("profile/:id", v1.GetProfile)
-
-		// 评论模块
-		router.POST("addcomment", v1.AddComment)
-		router.GET("comment/info/:id", v1.GetComment)
-		router.GET("commentfront/:id", v1.GetCommentListFront)
-		router.GET("commentcount/:id", v1.GetCommentCount)
 
 		router.GET("category/all", v1.GetAllCate)
 
